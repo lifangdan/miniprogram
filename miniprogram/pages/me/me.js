6b72a4be547f3c05bbf36ca1bfb28cd32487b7b6
lifangdan <845154579@qq.com>
2020-03-26 21:03:25 +0800

    Initial Commit

---
+++ miniprogram/pages/me/me.js
@@ -0,0 +1,83 @@
+// miniprogram/pages/me/me.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: wx.getStorageSync("userInfo")||{}
+  },
+  onGetUserInfo(e){
+      console.log(e)
+      let userInfo=e.detail.userInfo
+      //调用云函数获取用户openid
+      wx.cloud.callFunction({
+        name:'login',
+        complete:res=>{
+          userInfo.openid=res.result.openid;
+          //修改data里的userInfo参数
+          this.setData({
+            userInfo
+          })
+          //存储登录信息
+          wx.setStorageSync("userInfo", userInfo)
+          console.log(res)
+        }
+      })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
