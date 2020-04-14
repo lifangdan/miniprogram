b935356f60e979e1d007fd18c6a2bb1c08ac28ad
lifd <654321lfd>
2020-04-14 11:43:26 +0800

    个人资料添加图片上传、删除功能

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,8 +1,6 @@
 //app.js
 App({
-  openid: 'wxc3a062ade8d6f585',
-  onLaunch: function () {
-    
+  onLaunch: function () { 
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -11,46 +11,41 @@ Page({
     _id: 0,
     name: '',
     sex: '',
+    date: '',
+    record: '',
+    native: '',
+    address: '',
+    tempImg: '',
+    fileList: [],
+    fileIDs: [],
   },
   changeName(e) {
-    console.log(7878787878)
-    console.log(e.detail)
     this.data.name = e.detail;
-    console.log(this.data)
   },
   getData() {
-    let that=this
-    console.log(app.openid)
-    db.collection('profile').where({
-      _openid: app.openid
-}).get().then(res => {
-      console.log(888888888)
-      console.log(res.data)
+    let that = this
+    db.collection('profile').get().then(res => {
       let info = res.data[0];
       that.setData({
         name: info.name,
         sex: info.sex,
+        date: info.date,
+        record: info.record,
+        native: info.native,
+        address: info.address,
         _id: info._id
       })
-      console.log(this.data)
     })
   },
   update(e) {
     let that = this
-    console.log(111111)
-    console.log(that.data._id)
-    console.log(that.data.name)
     db.collection('profile').doc(that.data._id).update({
       data: {
         name: that.data.name,
       },
       success: function (res) {
-        console.log(8999999999)
-        console.log(res)
         db.collection('profile').doc(that.data._id).get({
           success: function (res) {
-            console.log(22222222)
-            console.log(res)
             wx.showToast({
               title: '修改成功',
               icon: 'success'
@@ -60,18 +55,112 @@ Page({
 
       }
     })
-    // db.collection('profile').update({
-    //   data:this.data.userData
-    // }).then(res => {
-    //   console.log(9999)
-    //   console.log(res.data)
-    // })
+  },
+  /**
+   * 上传
+   */
+  afterRead(event) {
+    let that = this;
+    console.log(event)
+    wx.cloud.uploadFile({
+      cloudPath: (new Date()).getTime() + '.png',
+      filePath: event.detail.file.path, // 文件路径
+    }).then(res => {
+      wx.cloud.getTempFileURL({
+        fileList: [res.fileID],
+        success: res => {
+          let fileList = res.fileList[0]
+          //将照片更新到云数据库中
+          db.collection('fileList').add({
+            data: {
+              url: fileList.tempFileURL,
+              name: (new Date()).getTime() + '.png',
+              fileID: '',
+              fileID: fileList.fileID
+            }
+          }).then(res => {
+            that.getTempFileURL()
+            wx.showToast({
+              title: '上传成功',
+            })
+
+          }).catch(error => {
+            console.log(error)
+          })
+        }, fail: err => {
+        }
+      })
+
+    }).catch(error => {
+      console.log('上传失败')
+      // handle error
+    })
+  },
+  /**
+   * 读取云数据库图片真实路径
+   */
+  getTempFileURL() {
+    let that = this;
+    db.collection('fileList').get().then(res => {
+      that.setData({
+        fileList: res.data,
+      })
+    })
+
+  },
+
+  delData(e){
+    let that = this
+    let fileID = e.detail.file.fileID
+    db.collection('fileList').doc(e.detail.file._id).remove({
+      success: function(res) {
+        console.log(res)
+        that.data.fileList.splice(that.data.fileList.findIndex(v => v.fileID == fileID), 1);
+        let newList = that.data.fileList
+        that.setData({
+          fileList: newList,
+        })
+        wx.showToast({
+          title: '删除成功',
+        })
+        that.delImg(e)
+      }
+    })
+  },
+    /**
+   * 删除云数据库图片
+   */
+  delImg(e) {
+    let fileID = e.detail.file.fileID
+    wx.cloud.deleteFile({
+      fileList: [fileID]
+    }).then(res => {
+    }).catch(error => {
+    })
+  },
+  /**
+   * 小程序自带上传方法
+   */
+  uploadImgHandle() {
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: res => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        this.setData({
+          tempImg: tempFilePaths
+        })
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     this.getData()
+    this.getTempFileURL()
   },
 
   /**
